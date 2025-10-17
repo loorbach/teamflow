bc70e12bbc9510b62fd5e9d35e6c4f2fd7d4aa01
loorbach <loorbachpl@gmail.com>
2025-10-17 11:17:41 +0200

    added auth redirect for /login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,15 @@
 import { SignInForm } from '@/components/signin-form'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
+  const session = await auth.api.getSession({ headers: await headers() })
+
+  if (session && session.user) {
+    redirect('/')
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
