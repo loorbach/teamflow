7d73df9064c8412ffeb32a5c6acb3addac1ceab3
Patrick Loorbach <loorbachpl@gmail.com>
2026-01-15 11:10:48 +0100

    updated eslint config and prettier formatting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,17 @@
 // eslint.config.mjs
-import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
-import nextTypescript from 'eslint-config-next/typescript'
-import prettier from 'eslint-config-prettier/flat'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['.next/**', 'out/**', 'build/**', 'node_modules/**', 'coverage/**']),
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'node_modules/**',
+    'coverage/**',
+  ]),
 
   ...nextCoreWebVitals,
   ...nextTypescript,
@@ -14,24 +20,33 @@ export default defineConfig([
   {
     rules: {
       // General code style
-      // quotes: ['error', 'single', { avoidEscape: true }],
-      // semi: ['error', 'always'],
-      // 'no-console': 'warn', // warn instead of error
+      semi: ['error', 'always'],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-console': 'warn', // warn instead of error
 
       // React-specific
-      'react/jsx-key': 'error', // ensure keys on lists
-      'react/react-in-jsx-scope': 'off', // Next 13+ doesn't need React import
+      'react/jsx-key': 'error', // keys in lists
+      'react/react-in-jsx-scope': 'off', // not needed in Next 13+
 
       // Next.js-specific
-      '@next/next/no-img-element': 'warn', // prefer <Image> but allow exceptions
-      '@next/next/no-html-link-for-pages': 'error', // avoid broken <a> links to pages
+      '@next/next/no-img-element': 'warn',
+      '@next/next/no-html-link-for-pages': 'error',
 
       // TypeScript
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-type': 'off', // optional
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
 
-      // Import ordering (requires eslint-plugin-import)
-      // 'import/order': ['error', { alphabetize: { order: 'asc' }, 'newlines-between': 'always' }],
+      // Import order (requires eslint-plugin-import)
+      'import/order': [
+        'error',
+        {
+          alphabetize: { order: 'asc' },
+          'newlines-between': 'always',
+        },
+      ],
     },
   },
-])
+]);
