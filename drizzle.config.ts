682a0fa75d807f825f15c2e64fc0edb999c8e428
Patrick Loorbach <loorbachpl@gmail.com>
2026-01-15 09:32:47 +0100

    DATABASE_URL check

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
 export default defineConfig({
   out: process.env.NODE_ENV === 'production' ? './drizzle' : './drizzle-local',
   schema: './db/schema.ts',
