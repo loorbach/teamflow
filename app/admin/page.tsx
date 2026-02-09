46c963d3519133943f1e3cd7e129439de2b5437e
Patrick Loorbach <loorbachpl@gmail.com>
2026-02-09 09:30:49 +0100

    added auth guard

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,15 @@
 import Header from '@/components/header';
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 async function Admin() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session || !session.user) {
+    redirect('/login');
+  }
+
   const users = await auth.api.listUsers({
     query: {},
     headers: await headers(),
